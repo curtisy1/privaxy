0b549e8740984f64686ff5ad2e0937e1c4476a84
curtisy <curtisy@mailbox.org>
2022-09-17 23:49:39 +0200

    feat(web): make navbar take full width

--- web_frontend/src/Navbar.tsx
+++ web_frontend/src/Navbar.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import {Card, Inline} from "braid-design-system";
+import {Card, Column, Columns, Inline} from "braid-design-system";
 import { Link } from "@tanstack/react-location";
 
 export default function Navbar() {
   return (
-    <Card>
-      <Inline space="gutter">
-        <nav>
+    <Columns space="small" collapseBelow="tablet">
+      <Column width="1/3">
+        <Card>
           <Link to="">Dashboard</Link>
+        </Card>
+      </Column>
+      <Column width="1/3">
+        <Card>
           <Link to="requests">Requests</Link>
+        </Card>
+      </Column>
+      <Column width="1/3">
+        <Card>
           <Link to="settings">Settings</Link>
-        </nav>
-      </Inline>
-    </Card>
+        </Card>
+      </Column>
+    </Columns>
   )
 }
